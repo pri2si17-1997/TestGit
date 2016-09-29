3e494cd851453e492a02ec747ed925e28f512703
pri2si17-1997 <pksinha217@gmail.com>
2016-09-29 12:14:36 +0530

    Initial Commit Just for testing
    Signed-off-by: pri2si17-1997 <pksinha217@gmail.com>

---
+++ toggleString.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string S;
+	string ans;
+	int a;
+	cin>>S;
+	for(int i = 0; i<S.length(); i++)
+	{
+		//printf("%d\n", (int)S.at(i));	
+		if(((int)S.at(i)) >= 97 && ((int)S.at(i)) <= 122)
+		{
+			//printf("%d\n", ((int)(S.at(i))-32));
+			ans += (char)((int)(S.at(i))-32);
+		}
+		else if(((int)S.at(i)) >= 65 && ((int)S.at(i)) <= 90)
+		{
+			ans += (char)((int)(S.at(i))+32);
+		}
+	}
+	cout<<ans<<endl;
+	return 0;
+}
